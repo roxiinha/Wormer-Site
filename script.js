afb1df5e4d7a2d935392bad1be95e778d78bfa5b
roxiinha <rochafx28@gmail.com>
2025-01-24 20:38:40 +0000

    .

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function enviarPedido(produto, preco) {
   const email = "kwormer77@gmail.com" // Substitua pelo e-mail onde deseja receber os pedidos
   const assunto = `Pedido de Compra - ${produto}`
-  const corpo = `Olá,\n\nGostaria de comprar o seguinte produto:\n\nProduto: ${produto}\nPreço: ${preco}\n\nPor favor, envie as instruções para pagamento via transferência bancária.\n\nObrigado.`
+  const corpo = `Olá,patrão!\n\nGostaria de comprar o seguinte produto:\n\nProduto: ${produto}\nPreço: ${preco}\n\nPor favor, envie me as instruções para pagamento via Mbway.\nMorada:  ( insira aqui a sua morada)\n\nObrigado.`
 
   // Abrir o Gmail com o e-mail preenchido
   window.location.href = `mailto:${email}?subject=${encodeURIComponent(
